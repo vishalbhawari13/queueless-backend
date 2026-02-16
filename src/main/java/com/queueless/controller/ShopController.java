748196f32c77b3761fa2ad247a01400ff7be8fe2
vishalbhawari53 <vishalbhawari53@gmail.com>
2026-02-16 16:40:23 +0530

    Shop Register api fix 2.0

--- src/main/java/com/queueless/controller/ShopController.java
+++ src/main/java/com/queueless/controller/ShopController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/shop")
+@RequestMapping("/api/shop")
 public class ShopController {
 
     private final ShopService shopService;
